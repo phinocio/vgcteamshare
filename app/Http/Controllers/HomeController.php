4ac7c2944ee34e3628db3edfbea055df33badf45
Phinocio <github@phinocio.com>
2019-12-10 17:38:02 -0700

    update some things and prepare coming soon page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,38 +11,23 @@ use App\Helpers\ShowdownExportParser;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-       // $this->middleware('auth');
-    }
+	/**
+	 * Create a new controller instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+	   // $this->middleware('auth');
+	}
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
-    public function index()
-    {
-        $data = 'Alakazam-Mega @ Alakazite  
-Ability: Trace  
-EVs: 104 HP / 44 Atk / 100 Def / 108 SpA / 4 SpD / 96 Spe  
-Timid Nature  
-IVs: 0 Atk  
-- Encore  
-- Calm Mind  
-- Dazzling Gleam  
-- Disable';
-
-      //  dd(urlencode($data));
-        $pokemon = ShowdownExportParser::parse($data);
-        dd($pokemon);
-        $team = Team::first();
-    
-		return new TeamResource($team);
-    }
+	/**
+	 * Show the application dashboard.
+	 *
+	 * @return \Illuminate\Contracts\Support\Renderable
+	 */
+	public function index()
+	{
+		return view('home');
+	}
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,40 @@
-@extends('layouts.app')
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-                    You are logged in!
-                </div>
+    <title>{{ config('app.name', 'VGCTeamshare') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="content">
+                <p class="brand">VGC Teamshare</p>
+            </div>
+            <div class="content">
+                <p>middle</p>
+            </div>
+            <div class="content">
+                <p>coming soon...</p>
             </div>
         </div>
     </div>
-</div>
-@endsection
+</body>
+
+</html>
