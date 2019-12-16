a0d751799aa9491d240d92aaa11ec4145a279aa9
Phinocio <github@phinocio.com>
2019-12-16 14:11:40 -0700

    extract upload form to blade component

---
+++ resources/views/components/uploadForm.blade.php
@@ -0,0 +1,32 @@
+<h2>New Team</h2>
+<hr />
+<form>
+	<div class="form-group">
+		<input type="text" class="form-control" id="teamName" aria-describedby="nameHelp" placeholder="Enter name">
+	</div>
+
+	<div class="form-group">
+		<select class="form-control" id="exampleFormControlSelect1">
+			<option>VGC 2020</option>
+			<option>2</option>
+			<option>3</option>
+			<option>4</option>
+			<option>5</option>
+		</select>
+	</div>
+
+	<div class="form-group">
+		<input type="text" class="form-control" id="rentalCode" aria-describedby="codeHelp" placeholder="Rental code...">
+	</div>
+
+	<div class="form-group">
+		<textarea class="form-control" id="description" aria-describedby="descriptionHelp" placeholder="Say a bit about the team..."></textarea>
+	</div>
+
+	<div class="form-group">
+		<textarea class="form-control" id="importTeam" rows="5" placeholder="Enter showdown export here..."></textarea>
+	</div>
+	<div class="text-right">
+		<button type="submit" class="btn btn-primary">Upload</button>
+	</div>
+</form>
