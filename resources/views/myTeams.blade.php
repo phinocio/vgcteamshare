9cc1c0d283072b916b2b5bc4b2e810783ff3a3cd
Phinocio <github@phinocio.com>
2019-12-17 14:54:19 -0700

    change to say my

--- resources/views/myTeams.blade.php
+++ resources/views/myTeams.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <h2>Your Teams</h2>
+            <h2>My Teams</h2>
             <hr />
             @include('components.teamPreview', ['teams' => $teams])
             {{ $paginate->links() }}
