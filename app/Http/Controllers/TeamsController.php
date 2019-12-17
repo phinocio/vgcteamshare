232be848ce02bd5fa6afbc4e8ceaa8f93d71bac6
Phinocio <github@phinocio.com>
2019-12-17 14:21:26 -0700

    create teams controller

---
+++ app/Http/Controllers/TeamsController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TeamsController extends Controller
+{
+    public function index() {
+        // dunno what do here. Search?
+    }
+
+    public function myTeams() {
+        
+    }
+}
