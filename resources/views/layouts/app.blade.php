4f3dcd7ce6a6d64fd8224f4c998364df9ba40a96
Phinocio <github@phinocio.com>
2019-12-17 14:21:37 -0700

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,9 +25,11 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-dark bg-primary shadow-sm">
             <div class="container">
+
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'VGCTeamshare') }}
+                    {{ config('app.name', 'VGCTeamshare') }}<sup class="beta">Beta</sup>
                 </a>
+
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
